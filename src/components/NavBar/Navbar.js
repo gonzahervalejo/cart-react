ac986abdc1f79a2c1745492db0bedb004e37f422
Your Name <you@example.com>
2022-09-26 16:53:13 -0300

    Clase 3

---
+++ src/components/NavBar/Navbar.js
@@ -0,0 +1,19 @@
+import './Navbar.css'
+
+
+const Navbar = () => {
+  return (
+    <nav className='navBar'>
+        Ziba medias
+
+        <div>
+            <button>   Inicio  </button>
+            <button>   Categorias </button>
+            <button>   Nosotros  </button>
+            
+        </div>
+    </nav>
+  )
+}
+export default Navbar
+
